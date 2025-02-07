ef2f58bee09cc9c9567d49b1f911706b37b3beca
Joery <44531907+Joery-M@users.noreply.github.com>
2025-02-07 11:41:56 +0100

    refactor: build with isolated declarations

--- libraries/prang/rollup.config.js
+++ libraries/prang/rollup.config.js
@@ -1,11 +1,9 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import { join } from 'path';
 import { premove } from 'premove';
-import dts from 'rollup-plugin-dts';
 import { defineConfig } from 'rollup';
 import esbuild from 'rollup-plugin-esbuild';
+import UnpluginIsolatedDecl from 'unplugin-isolated-decl/rollup';
 
 const banner = `/**
  * @prang/core
@@ -16,54 +14,23 @@ const banner = `/**
 
 const external = ['rxjs', '@vue/reactivity', '@vue/runtime-dom', '@vue/shared', '@vueuse/rxjs'];
 
-export default defineConfig([
-    // Generate types
-    {
-        input: ['./src/index.ts', './src/runtime.ts', './src/rxjs.ts'],
-        external,
-        plugins: [
-            {
-                async buildStart() {
-                    await premove(join(import.meta.dirname, './temp/'));
-                }
-            },
-            typescript({
-                tsconfig: './tsconfig.json'
-            })
-        ],
-        output: { dir: 'temp' }
-    },
-    // Bundle types
-    {
-        input: ['./temp/index.d.ts', './temp/runtime.d.ts', './temp/rxjs.d.ts'],
-        plugins: [
-            dts(),
-            {
-                async buildEnd() {
-                    await premove(join(import.meta.dirname, './temp/'));
-                }
-            }
-        ],
-        output: {
-            entryFileNames: (chunk) => {
-                return chunk.name.replace(/temp(\\|\/)/, '') + '.d.ts';
-            },
-            banner,
-            dir: 'dist',
-            format: 'esm'
-        }
-    },
-    // Generate TS
-    {
-        input: ['./src/index.ts', './src/runtime.ts', './src/rxjs.ts'],
-        plugins: [resolve(), esbuild(), commonjs()],
-        treeshake: true,
-        external,
-        output: {
-            entryFileNames: (chunk) => chunk.name + '.js',
-            banner,
-            dir: 'dist',
-            format: 'esm'
-        }
+/**
+ * @type {import('rollup').Plugin}
+ */
+const cleanPlugin = {
+    async buildStart() {
+        await premove('./dist/', { cwd: import.meta.dirname });
     }
-]);
+};
+
+export default defineConfig({
+    input: ['./src/index.ts', './src/runtime.ts', './src/rxjs.ts'],
+    plugins: [cleanPlugin, UnpluginIsolatedDecl(), resolve(), esbuild(), commonjs()],
+    treeshake: true,
+    external,
+    output: {
+        banner,
+        dir: 'dist',
+        format: 'esm'
+    }
+});
--- libraries/vite-plugin-prang/rollup.config.js
+++ libraries/vite-plugin-prang/rollup.config.js
@@ -1,8 +1,10 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import { premove } from 'premove';
 import { defineConfig } from 'rollup';
-import { dts } from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
+import UnpluginIsolatedDecl from 'unplugin-isolated-decl/rollup';
+import packageJson from './package.json' assert { type: 'json' };
 
 const banner = `/**
  * vite-plugin-prang
@@ -11,34 +13,23 @@ const banner = `/**
  */
 `;
 
-export default defineConfig([
-    {
-        input: './src/index.ts',
-        plugins: [dts()],
-        output: { file: 'dist/index.d.ts', format: 'es' }
-    },
-    {
-        input: './src/index.ts',
-        plugins: [resolve(), esbuild(), commonjs()],
-        treeshake: true,
-        external: [
-            '@babel/generator',
-            '@babel/parser',
-            '@babel/types',
-            '@vue/compiler-core',
-            '@vue/compiler-dom',
-            '@vue/compiler-sfc',
-            '@vue/shared',
-            'entities',
-            'magic-string',
-            'rollup',
-            'vite'
-        ],
-        output: {
-            entryFileNames: (chunk) => chunk.name + '.js',
-            banner,
-            dir: 'dist',
-            format: 'esm'
-        }
+/**
+ * @type {import('rollup').Plugin}
+ */
+const cleanPlugin = {
+    async buildStart() {
+        await premove('./dist/', { cwd: import.meta.dirname });
     }
-]);
+};
+
+export default defineConfig({
+    input: './src/index.ts',
+    plugins: [cleanPlugin, UnpluginIsolatedDecl(), resolve(), esbuild(), commonjs()],
+    treeshake: true,
+    external: [...Object.keys(packageJson.dependencies), ...Object.keys(packageJson.peerDependencies)],
+    output: {
+        banner,
+        dir: 'dist',
+        format: 'esm'
+    }
+});
